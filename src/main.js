125c75e1d070d848e094fe2671a3c06247989e26
Jonathan Costa <jonathanduartecosta@gmail.com>
2023-01-17 21:48:43 -0300

    criando a sessão atrações

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-document.addEventListener("DOMContentLoaded"),
-  function () {
-    const buttons = document.querySelectorAll("[data-tab-button]");
+document.addEventListener("DOMContentLoaded", (event) => {
+  const buttons = document.querySelectorAll("[data-tab-button]");
 
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener("click", function (botao) {
-        const abaAlvo = botao.target.dataset.tabButton;
-        const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
-        escondeTodasAbas();
-        aba.classList.add("shows__list--is-active");
-        removeBotaoAtivo();
-        botao.target.classList.add("shows__tabs__button--is-active");
-      });
-    }
-  };
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function (botao) {
+      const abaAlvo = botao.target.dataset.tabButton;
+      const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
+      escondeTodasAbas();
+      aba.classList.add("shows__list--is-active");
+      removeBotaoAtivo();
+      botao.target.classList.add("shows__tabs__button--is-active");
+    });
+  }
+});
 
 function removeBotaoAtivo() {
   const buttons = document.querySelectorAll("[data-tab-button]");
